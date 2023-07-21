389a279541f471bef38721cb4c5352e221027e31
WilhelmK109 <kamulungaw@gmail.com>
2023-07-22 00:16:04 +0200

    Implement a method #each that yields successive members of @list

---
+++ my_list.rb
@@ -0,0 +1,14 @@
+# my_list.rb
+require_relative 'my_enumerable'
+
+class MyList
+  include MyEnumerable
+
+  def initialize(*items)
+    @list = items
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
